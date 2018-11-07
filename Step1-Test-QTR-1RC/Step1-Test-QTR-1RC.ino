a8a12c06c5616a501779c42d76d8fc0795afa5b3
juandgainesSBLM <34096059+juandgainesSBLM@users.noreply.github.com>
2018-11-06 19:42:57 -0500

    QTR test
    QTR test

---
+++ Step1-Test-QTR-1RC/Step1-Test-QTR-1RC.ino
@@ -0,0 +1,26 @@
+
+int sensor1;
+int sensor2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor1 = digitalRead(2);
+  sensor2 = digitalRead(3);
+
+  Serial.print("Right QTR sensor:");
+  Serial.print(sensor2);
+  Serial.print("| Left QTR sensor:");
+  Serial.print(sensor1);
+
+  delay(100);
+
+}
